84aebc1839668e1c4a64e9aa4ea8e8e08c105b95
fucker <fucker@pornhub.com>
2021-09-22 12:17:07 +0800

    update

--- login.go
+++ login.go
@@ -230,7 +230,7 @@ func init() {
 			},
 		},
 		{
-			Rules: []string{`raw ^\d{6}$`},
+			Rules: []string{`raw ^(\d{6})$`},
 			Admin: true,
 			Handle: func(s core.Sender) interface{} {
 				if code, ok := codes[s.GetImType()+fmt.Sprint(s.GetUserID())]; ok {
