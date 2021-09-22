888f845bcb5d22fd1dd665580ce4a2f8c9114fe4
fucker <fucker@pornhub.com>
2021-09-22 13:39:23 +0800

    update

--- login.go
+++ login.go
@@ -142,7 +142,7 @@ func init() {
 		{
 			Rules: []string{`raw ^(\d{11})$`},
 			Handle: func(s core.Sender) interface{} {
-				if jd_cookie.Get("igtg", false) == "true" {
+				if jd_cookie.Get("igtg", false) == "true" && s.GetImType() == "tg" {
 					return "滚，不欢迎你。"
 				}
 				if num := jd_cookie.GetInt("login_num", 2); len(codes) >= num {
