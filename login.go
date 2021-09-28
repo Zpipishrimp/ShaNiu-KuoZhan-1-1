2b74b14c2565864e616697a6f51ea93f9d1f5906
fucker <fucker@pornhub.com>
2021-09-28 17:39:44 +0800

    update

--- login.go
+++ login.go
@@ -273,7 +273,7 @@ func init() {
 										s.Reply(errors.New("对不起，短信验证码请求频繁，请稍后再试。"), core.E)
 										return
 									}
-									if query.AuthCodeCountDown <= 0 {
+									if query.AuthCodeCountDown < 0 {
 										s.Reply("验证码超时，登录失败。", core.E)
 										return
 									} else {
