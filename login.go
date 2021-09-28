197f3fd83162d5bf4975b709b5c75657262ff44c
fucker <fucker@pornhub.com>
2021-09-28 17:50:25 +0800

    update

--- login.go
+++ login.go
@@ -277,9 +277,9 @@ func init() {
 										s.Reply("验证码超时，登录失败。", core.E)
 										return
 									} else if query.AuthCodeCountDown > 0 {
-										if s.GetImType() == "tg" {
-											s.Reply(fmt.Sprintf("验证码倒计时：%d秒。", query.AuthCodeCountDown), core.E)
-										}
+										// if s.GetImType() == "tg" {
+										// 	s.Reply(fmt.Sprintf("验证码倒计时：%d秒。", query.AuthCodeCountDown), core.E)
+										// }
 									}
 
 								}
