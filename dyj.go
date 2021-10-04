448235e26d9f85670adec0750fce2540fc7c47ee
fucker <fuck@pornhub.com>
2021-10-04 20:49:55 +0800

    update

--- dyj.go
+++ dyj.go
@@ -56,7 +56,7 @@ func init() {
 				s := 1
 				for i := 0; i < len(envs); i++ {
 					if envs[i].Status == 0 {
-						req := httplib.Get("https://api.m.jd.com/?functionId=openRedEnvelopeInteract&body=" + `{"linkId":"yMVR-_QKRd2Mq27xguJG-w","redEnvelopeId":"48855bbdac3443bfbbc5d6cd34c9ef4566211633281100281","inviter":"WaHpHMRJdh28jqa9WTwWl3LXebXXp5CBbOkCxVi4jTg","helpType":"` + fmt.Sprint(s) + `"}` + "&t=" + fmt.Sprint(time.Now().Unix()) + "&appid=activities_platform&clientVersion=3.5.6")
+						req := httplib.Get("https://api.m.jd.com/?functionId=openRedEnvelopeInteract&body=" + `{"linkId":"yMVR-_QKRd2Mq27xguJG-w","redEnvelopeId":"` + redEnvelopeId + `","inviter":"` + inviterId + `","helpType":"` + fmt.Sprint(s) + `"}` + "&t=" + fmt.Sprint(time.Now().Unix()) + "&appid=activities_platform&clientVersion=3.5.6")
 						req.Header("Cookie", envs[i].Value)
 						req.Header("Accept", "*/*")
 						req.Header("Connection", "keep-alive")
